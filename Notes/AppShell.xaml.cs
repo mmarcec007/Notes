3ae3ddad388c1810d8b67aac0985ed8065aa40ac
Mark Marčec <mark.marcec@gmail.com>
2023-09-04 22:46:24 +0200

    Initial working app demo

--- Notes/AppShell.xaml.cs
+++ Notes/AppShell.xaml.cs
@@ -5,6 +5,8 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Pages.NotePage), typeof(Pages.NotePage));
         }
     }
 }
--- Notes/Pages/NotePage.xaml.cs
+++ Notes/Pages/NotePage.xaml.cs
@@ -1,9 +1,55 @@
 namespace Notes.Pages;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-	public NotePage()
+
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+
+    public NotePage()
 	{
 		InitializeComponent();
-	}
+
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.FileName, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.FileName))
+                File.Delete(note.FileName);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.FileName = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
 }
